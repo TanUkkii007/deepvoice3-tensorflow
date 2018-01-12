1595af09d3ff825b20fa9265c64ad9a65e7a2998
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2018-01-13 04:17:35 +0900

    add lint and dependency settings

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+setup(name='deepvoice3_tensorflow',
+      version=version,
+      description='',
+      packages=find_packages(),
+      install_requires=[
+          "numpy",
+      ],
+      extras_require={
+          "test": ["pylint"]
+      }
+     )
