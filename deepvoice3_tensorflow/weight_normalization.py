fix weight normalization to handle rank 2 tensor

deepvoice3_tensorflow/weight_normalization.py
@@ -7,6 +7,8 @@ class WeightNormalization(tf.layers.Layer):
             trainable=trainable, name=name, **kwargs)
         self.weight_value = weight_value
         self.dimension = dimension
+        self.ndims = self.weight_value.shape.ndims
+        self.reduction_axis = self._compute_reduction_axis()
 
     def build(self, weight_shape):
         # add g and v as new parameters and express w as g/||v|| * v
@@ -18,18 +20,21 @@ class WeightNormalization(tf.layers.Layer):
         return self.compute_weight()
 
     def initial_g(self):
-        norm = tf.norm(self.weight_value, axis=self.dimension)
+        norm = tf.norm(self.weight_value, axis=self.reduction_axis)
         return norm
 
     def compute_weight(self):
         g = self.g
         v = self.v
-        gv = (g / tf.norm(v, axis=self.dimension))
-        # expand dimension in addition to batch
-        gv = tf.reshape(gv, shape=[-1] + [1 for _ in self.dimension])
+        gv = (g / tf.norm(v, axis=self.reduction_axis))
+        # expand dimension in addition to batch. ToDo: generalize
+        gv = tf.reshape(gv, shape=[-1] + [1 for _ in range(self.ndims - 1)])
         return v * gv
 
+    def _compute_reduction_axis(self):
+        r = [i for i in range(0, self.ndims) if i != self.dimension]
+        return r[0] if len(r) == 1 else r
 
-def weight_normalization(weight, dimension=(1,2)):
+def weight_normalization(weight, dimension=0):
     wn = WeightNormalization(weight.initialized_value(), dimension)
     return wn.apply(weight)

tests/weight_normalization_test.py
@@ -8,14 +8,48 @@ from deepvoice3_tensorflow.weight_normalization import weight_normalization, Wei
 
 class WeightNormalizationTest(tf.test.TestCase):
 
+    @given(_weight=arrays(dtype=np.float32, shape=[4, 5], elements=integers(-5, 5)),
+           input=arrays(dtype=np.float32, shape=[5, 1], elements=integers(-10, 10), unique=True))
+    @settings(max_examples=10, timeout=unlimited)
+    def test_weight_normalization_2(self, _weight, input):
+        assume(not np.all(_weight == 0.0))
+        input_pf = tf.placeholder(dtype=tf.float32, shape=[5, 1])
+        weight = tf.Variable(_weight, trainable=False)
+        wn = WeightNormalization(weight.initialized_value(), dimension=0)
+        normalized_weight = wn.apply(weight)
+        output = tf.reduce_sum(tf.matmul(normalized_weight, input_pf), axis=0)
+        output_original = tf.reduce_sum(tf.matmul(weight, input_pf), axis=0)
+
+        def zero_gradient_if_none(grad):
+            return [tf.zeros_like(output) if g is None else g for g in grad]
+
+        grad_g = zero_gradient_if_none(tf.gradients(output, wn.g))
+        grad_v = zero_gradient_if_none(tf.gradients(output, wn.v))
+        grad_w = zero_gradient_if_none(tf.gradients(output, weight))
+        grad_w_original = zero_gradient_if_none(tf.gradients(output_original, weight))
+
+        with self.test_session() as sess:
+            sess.run(tf.global_variables_initializer())
+
+            output_value, grad_w_value, grad_g_value, grad_v_value, grad_w_original_value = sess.run(
+                [output, grad_w, grad_g, grad_v, grad_w_original], feed_dict={input_pf: input})
+            # assert no dependency on weight
+            self.assertAllEqual(np.zeros_like(output_value), grad_w_value[0])
+
+            v = _weight
+            v_norm = np.linalg.norm(v, axis=1)
+            # \nabla_g L = \sum_{i,j}\frac{\partial L}{\partial w_{ij}}\nabla_g w_{i,j}
+            self.assertAllClose(grad_g_value[0],
+                                np.sum(grad_w_original_value[0] * v, axis=1) / v_norm)
+
     @given(_weight=arrays(dtype=np.float32, shape=[3, 4, 5], elements=integers(-5, 5)),
            input=arrays(dtype=np.float32, shape=[3, 5, 1], elements=integers(-10, 10), unique=True))
     @settings(max_examples=10, timeout=unlimited)
-    def test_weight_normalization(self, _weight, input):
+    def test_weight_normalization_3(self, _weight, input):
         assume(not np.all(_weight == 0.0))
         input_pf = tf.placeholder(dtype=tf.float32, shape=[3, 5, 1])
         weight = tf.Variable(_weight, trainable=False)
-        wn = WeightNormalization(weight.initialized_value(), dimension=(1, 2))
+        wn = WeightNormalization(weight.initialized_value(), dimension=0)
         normalized_weight = wn.apply(weight)
         output = tf.reduce_sum(tf.matmul(normalized_weight, input_pf), axis=0)
         output_original = tf.reduce_sum(tf.matmul(weight, input_pf), axis=0)
@@ -40,4 +74,4 @@ class WeightNormalizationTest(tf.test.TestCase):
             v_norm = np.linalg.norm(v, axis=(1, 2))
             # \nabla_g L = \sum_{i,j}\frac{\partial L}{\partial w_{ij}}\nabla_g w_{i,j}
             self.assertAllClose(grad_g_value[0],
-                                np.sum(grad_w_original_value[0] * v, axis=(1, 2)) / v_norm)
+                                np.sum(grad_w_original_value[0] * v, axis=(1,2)) / v_norm)