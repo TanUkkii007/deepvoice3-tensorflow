9ece039a0ff1f30549991e8f7eeda518e6b6acbb
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2018-04-15 14:13:59 +0900

    test weak orthogonality of sinusoidal encoding

--- tests/positional_encoding_graph_test.py
+++ tests/positional_encoding_graph_test.py
@@ -27,5 +27,19 @@ class PositionalEncodingTest(tf.test.TestCase):
             print("max diff", np.max(np.abs(y - x)))
             self.assertAllClose(x, y, atol=1e-4)
 
+    @given(n_positions=integers(2, 10), dimension=integers(2, 128))
+    @settings(max_examples=10, timeout=unlimited)
+    def test_weakly_orthogonal(self, n_positions, dimension):
+        assume(dimension % 2 == 0)
+
+        pe = PositionalEncoding.initial_value(n_positions, dimension)
+        x = pe.sinusoidal_encode().value[1:, :]  # drop position 0
+        xx = tf.matmul(x, tf.transpose(x))
+        with self.test_session() as sess:
+            xx = sess.run(xx)
+            eye = dimension // 2 * np.eye(n_positions)[1:, 1:]
+            # if x is orthogonal, xx == eye, but x is not exactly orthogonal so only check the diagonal
+            self.assertAllClose(np.diag(eye), np.diag(xx))
+
 if __name__ == '__main__':
     tf.test.main()
