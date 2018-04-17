da6f8e095e467ce4234267e8d5d6900a59637b9b
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2018-04-17 21:48:18 +0900

    SinusoidalEncodingEmbedding test

---
+++ tests/sinusoidal_encoding_embedding_graph_test.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+from deepvoice3_tensorflow.modules import SinusoidalEncodingEmbedding
+from hypothesis import given, settings, unlimited, assume
+from hypothesis.strategies import integers
+import numpy as np
+
+
+class SinusoidalEncodingEmbeddingTest(tf.test.TestCase):
+
+    @given(n_positions=integers(2, 20), dimension=integers(2, 128))
+    @settings(max_examples=10, timeout=unlimited)
+    def test_weakly_orthogonal(self, n_positions, dimension):
+        assume(dimension % 2 == 0)
+
+        see = SinusoidalEncodingEmbedding(n_positions, dimension)
+        positions = tf.range(1, n_positions)
+        x = see(positions)
+        xx = tf.matmul(x, tf.transpose(x))
+        with self.test_session() as sess:
+            sess.run(tf.global_variables_initializer())
+            xx = sess.run(xx)
+            eye = dimension // 2 * np.eye(n_positions)[1:, 1:]
+            # if x is orthogonal, xx == eye, but x is not exactly orthogonal so only check the diagonal
+            self.assertAllClose(np.diag(eye), np.diag(xx))
+
+    @given(n_positions=integers(2, 20), dimension=integers(2, 128))
+    @settings(max_examples=10, timeout=unlimited)
+    def test_speaker_rate(self, n_positions, dimension):
+        assume(dimension % 2 == 0)
+
+        see = SinusoidalEncodingEmbedding(n_positions, dimension)
+        positions = tf.range(1, n_positions)
+        positions_half = tf.range(1, n_positions // 2)
+        x = see(positions)
+        x2 = see(positions_half, w=2)
+        x2x = tf.matmul(x, tf.transpose(x2))
+        with self.test_session() as sess:
+            sess.run(tf.global_variables_initializer())
+            x2x = sess.run(x2x)
+            print(x2x)
+
+if __name__ == '__main__':
+    tf.test.main()
